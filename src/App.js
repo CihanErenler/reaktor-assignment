bc666c607c1600cb4cdfc87efef0afbc50002764
cihanerenler <cihanerenler@outlook.com>
2022-12-10 00:36:01 +0200

    fix sass

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./Styles/App.scss";
+import "./Styles/App.css";
 import AppLayout from "./Components/AppLayout";
 
 function App() {
-	return (
-		<main className="App">
-			<h1 className="main-title">Drone Tracking Dashboard</h1>
-			<div className="app-container">
-				<AppLayout />
-			</div>
-		</main>
-	);
+  return (
+    <main className="App">
+      <h1 className="main-title">Drone Tracking Dashboard</h1>
+      <div className="app-container">
+        <AppLayout />
+      </div>
+    </main>
+  );
 }
 
 export default App;
